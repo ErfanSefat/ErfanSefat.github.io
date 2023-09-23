88a14ea3638c724b46a3965e7aff366b61bf3676
Erfan <Erfan@sth.com>
2023-09-23 17:11:50 +0330

    my animojy gone/animation on contact elements/magnet on button and intro/projest slider from scratch with 5 slides

--- script.js
+++ script.js
@@ -49,3 +49,76 @@ function callMe(){
 function telegramMe(){
     window.open("https://t.me/ErfanSefat");
 }
+{
+    const boxes = document.querySelectorAll(".magnet");
+    boxes.forEach((box) => {
+        box.addEventListener('mousemove', (e) => {
+            let x = e.offsetX;
+            let y = e.offsetY;
+            let boxWidth = box.clientWidth;
+            let boxHeight = box.clientHeight;
+            let moveX = (x - boxWidth/2)/5;
+            let moveY = (y - boxHeight/2)/5;
+            box.style.transform = `translateX(${moveX}px)
+            translateY(${moveY}px)`;
+        })
+        box.addEventListener("mouseout", (e) => {
+            box.style.transform = ``;
+        })
+    })
+    
+}
+{
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting)
+            {
+                entry.target.classList.add("show");
+            }else{
+                entry.target.classList.remove("show");
+            }
+        });
+    });
+    const hiddenItems = document.querySelectorAll(".hidden");
+    hiddenItems.forEach((item) => {
+        observer.observe(item);
+    });
+}
+function next(){
+    const selected = document.querySelector(".selected");
+    const unselected_R = document.querySelector(".unselected-right");
+    const unselected_L = document.querySelector(".unselected-left");
+    const unselected_R_R = document.querySelector(".unselected-right-right");
+    const unselected_L_L = document.querySelector(".unselected-left-left");
+
+    unselected_L_L.classList.add('unselected-right-right');
+    unselected_L.classList.add('unselected-left-left');
+    selected.classList.add('unselected-left');
+    unselected_R.classList.add('selected');
+    unselected_R_R.classList.add('unselected-right');
+
+    unselected_L_L.classList.remove('unselected-left-left');
+    unselected_L.classList.remove('unselected-left');
+    selected.classList.remove('selected');
+    unselected_R.classList.remove('unselected-right');
+    unselected_R_R.classList.remove('unselected-right-right');
+}
+function previous(){
+    const selected = document.querySelector(".selected");
+    const unselected_R = document.querySelector(".unselected-right");
+    const unselected_L = document.querySelector(".unselected-left");
+    const unselected_R_R = document.querySelector(".unselected-right-right");
+    const unselected_L_L = document.querySelector(".unselected-left-left");
+
+    unselected_L_L.classList.add('unselected-left');
+    unselected_L.classList.add('selected');
+    selected.classList.add('unselected-right');
+    unselected_R.classList.add('unselected-right-right');
+    unselected_R_R.classList.add('unselected-left-left');
+
+    unselected_L_L.classList.remove('unselected-left-left');
+    unselected_L.classList.remove('unselected-left');
+    selected.classList.remove('selected');
+    unselected_R.classList.remove('unselected-right');
+    unselected_R_R.classList.remove('unselected-right-right');
+}
